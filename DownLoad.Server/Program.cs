ff042edc14216c87178c420db12df156d1d5d3a8
Anton Firszov <antonfir@gmail.com>
2021-04-15 16:39:51 +0200

    http2

--- DownLoad.Server/Program.cs
+++ DownLoad.Server/Program.cs
@@ -21,11 +21,28 @@ namespace DownLoad.Server
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
-                    webBuilder.ConfigureKestrel(options =>
-                    {
-                        options.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http2);
-                    });
+                    bool http2 = args.Any(a => a.ToLower().Contains("http2"));
+                    webBuilder
+                        .UseStartup<Startup>()
+                        .UseKestrel(options =>
+                        {
+                            if (http2)
+                            {
+                                options.ListenLocalhost(5001);
+                            }
+                            else
+                            {
+                                options.ListenLocalhost(5000);
+                            }
+                        })
+                        .ConfigureKestrel(options =>
+                        {
+                            if (http2)
+                            {
+                                options.ConfigureEndpointDefaults(lo => lo.Protocols = HttpProtocols.Http2);
+                                Console.WriteLine("Configured for HTTP2!");
+                            }
+                        });
                 });
     }
 }
